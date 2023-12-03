062fe4fc1e2f361dfbfc0baa0b3476f4fa31ec65
Justin10612 <atom.9031@gmail.com>
2023-12-04 01:56:32 +0800

    Create a idle_mode detector to clear output

--- src/diff_drive_controller.cpp
+++ src/diff_drive_controller.cpp
@@ -17,6 +17,8 @@ const double robot_width = 0.35;       // meter
 const double wheel_radius = 0.0625; // meter
 const double b = robot_width/2.0;
 
+// bool idle_state = true;
+
 class DiffDriveController : public rclcpp::Node
 {
   public:
@@ -25,12 +27,14 @@ class DiffDriveController : public rclcpp::Node
       wheel_cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Vector3>("wheel_cmd_vel", 10);
       cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
         "cmd_vel", 10, std::bind(&DiffDriveController::cmd_vel_callback, this, std::placeholders::_1));
+      // idle_mode_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+      //   "idle_bool", 10, std::bind(&DiffDriveController::idle_callback, this, std::placeholders::_1));
     }
 
   private:
     rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr wheel_cmd_vel_pub_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr idle_mode_pub;
+    // rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr idle_mode_sub_;
     
     void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr cmd_message) const
     {
@@ -47,6 +51,17 @@ class DiffDriveController : public rclcpp::Node
       wheel_cmd_vel_pub_->publish(out_message);
     }
 
+    // void idle_callback(const std_msgs::msg::Bool::SharedPtr idle_msgs) const
+    // {
+    //   idle_state = idle_msgs->data;
+    //   if(idle_state==true){
+    //     auto out_message = geometry_msgs::msg::Vector3();
+    //     out_message.x = 0;  // left wheel speed
+    //     out_message.y = 0; // right wheel speed
+    //     // Publish data
+    //     wheel_cmd_vel_pub_->publish(out_message);
+    //   }
+    // }
 };
 
 int main(int argc, char * argv[])
